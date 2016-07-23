0420d45a73953c3e68626f4bee28395beec2ce0b
William Barbosa <william.sb@hotmail.com>
2016-07-23 14:24:55 -0300

    :lipstick: Better naming

--- Fabric.Crashlytics/Additions/DeviceTokenLoader.cs
+++ Fabric.Crashlytics/Additions/DeviceTokenLoader.cs
@@ -6,7 +6,7 @@ namespace Com.Crashlytics.Android.Beta
 	public partial class DeviceTokenLoader
 	{
 		[Register("Load", "(Landroid/content/Context;)Ljava/lang/String;", "GetLoad_Landroid_content_Context_Handler")]
-		public virtual Object Load(Context p0)
-			=> (Object)JavaLoad(p0);
+		public virtual Object Load(Context context)
+			=> (Object)JavaLoad(context);
 	}
 }
--- Fabric.TwitterCore/Additions/ScribeEvent.cs
+++ Fabric.TwitterCore/Additions/ScribeEvent.cs
@@ -8,8 +8,8 @@ namespace Com.Twitter.Sdk.Android.Core.Internal.Scribe
 		public partial class Transform
 		{
 			[Register("toBytes", "(Lcom/twitter/sdk/android/core/internal/scribe/ScribeEvent;)[B", "GetToBytes_Lcom_twitter_sdk_android_core_internal_scribe_ScribeEvent_Handler")]
-			public virtual byte[] ToBytes(Object p0)
-				=> ToBytes((ScribeEvent)p0);
+			public virtual byte[] ToBytes(Object scribeEvent)
+				=> ToBytes((ScribeEvent)scribeEvent);
 		}
 	}
 }
