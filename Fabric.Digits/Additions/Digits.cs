1285d15a008e42728ca1f7bff264c1bfce7bb337
William Barbosa <william.sb@hotmail.com>
2016-07-23 14:24:25 -0300

    :sparkles: Digits Kit

---
+++ Fabric.Digits/Additions/Digits.cs
@@ -0,0 +1,16 @@
+﻿using Android.Runtime;
+using Java.Lang;
+
+namespace Com.Digits.Sdk.Android
+{
+	public partial class Digits
+	{
+		public override int CompareTo(Object another)
+			=> CompareTo((IO.Fabric.Sdk.Android.Kit)another);
+	
+		[Register("doInBackground", "()Ljava/lang/Object;", "GetDoInBackgroundHandler")]
+		protected override unsafe global::Java.Lang.Object DoInBackground()
+			=> JavaDoInBackground();
+	}
+}
+
---
+++ Fabric.Digits/Additions/DigitsSession.cs
@@ -0,0 +1,19 @@
+﻿using Android.Runtime;
+using Java.Lang;
+
+namespace Com.Digits.Sdk.Android
+{
+	public partial class DigitsSession
+	{
+		public partial class Serializer
+		{
+			[Register ("deserialize", "(Ljava/lang/String;)Lcom/digits/sdk/android/DigitsSession;", "GetDeserialize_Ljava_lang_String_Handler")]
+			public virtual Object Deserialize(string serializedSession)
+				=> (Object)JavaDeserialize(serializedSession);
+
+			[Register ("serialize", "(Lcom/digits/sdk/android/DigitsSession;)Ljava/lang/String;", "GetSerialize_Lcom_digits_sdk_android_DigitsSession_Handler")]
+			public virtual unsafe string Serialize (Object session)
+				=> JavaSerialize((DigitsSession)session);
+		}
+	}
+}
